865f917b042763e0debeac6bab9ef4a3fb55764f
Riddhick <riddhick14@gmail.com>
2023-05-17 21:34:45 +0530

    Home page added

--- php/Home.php
+++ php/Home.php
@@ -1,5 +1,18 @@
 <?php
     session_start();
-    echo $_SESSION["name"];
-    echo $_SESSION["id"];
+    //echo $_SESSION["name"];
+    //echo $_SESSION["id"];
 ?>
+<html>
+<head>
+    <title>Main</title>
+    <link rel="stylesheet" link="/ChatApp/css/profile.css">
+</head>
+<body>
+    <div class="container">
+        <div class="pannel left"></div>
+        <div class="pannel center"></div>
+        <div class="pannel right"></div>
+    </div>
+</body>
+</html>
