0be498804440209e0b83da91b9cf517804f5a31a
Riddhick <riddhick14@gmail.com>
2023-05-17 22:11:45 +0530

    Changes in landing page JS

--- script/script.js
+++ script/script.js
@@ -3,3 +3,12 @@ function SignUp(){
     document.getElementById('login-signup-form').style.display="none";
     document.getElementById('signup').style.display="flex";
 }
+function Login(){
+    document.getElementById('login-signup-form').style.display="none";
+    document.getElementById('login').style.display="flex";
+}
+
+function Back(obj){
+    obj.style.display="none";
+    document.getElementById('login-signup-form').style.display="flex";
+}
