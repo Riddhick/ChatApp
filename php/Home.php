b0963badf179ee11bb8333f06d3ad1e4cba6f606
Riddhick <riddhick14@gmail.com>
2023-05-20 23:21:53 +0530

    Home page setup

--- php/Home.php
+++ php/Home.php
@@ -6,13 +6,16 @@
 <html>
 <head>
     <title>Main</title>
-    <link rel="stylesheet" link="/ChatApp/css/profile.css">
+    <link rel="stylesheet" href="/ChatApp/css/profile.css">
 </head>
 <body>
     <div class="container">
-        <div class="pannel left"></div>
-        <div class="pannel center"></div>
-        <div class="pannel right"></div>
+        <div class="pannel left">Friend List</div>
+        <div class="pannel center">
+            <div class="name-bar"><?php echo $_SESSION["name"]  ?>
+        </div>
+        </div>
+        <div class="pannel right">Settings</div>
     </div>
 </body>
 </html>
