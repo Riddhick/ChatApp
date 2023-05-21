672346bd7909758d7c6d8f2515b20df477aef866
Riddhick <riddhick14@gmail.com>
2023-05-21 19:58:39 +0530

    message button added in friend list

--- php/Home.php
+++ php/Home.php
@@ -13,16 +13,19 @@
         <div class="pannel left">
             <div class="heading-left">Friend List</div>
             <div class="friendlist">
-            <?php $mysqli=require __DIR__."/db_connection.php";
-            $sql="SELECT name FROM user WHERE id!='$user_id'";
-            $records=mysqli_query($conn,$sql);
-            foreach ($records as $record){
-                //$row=mysqli_fetch_assoc($record);
-                echo $record["name"];?>
-                <br>
-            <?php
-            } 
-            ?></div>
+                
+                    <?php $mysqli=require __DIR__."/db_connection.php";
+                    $sql="SELECT name FROM user WHERE id!='$user_id'";
+                    $records=mysqli_query($conn,$sql);
+                    foreach ($records as $record){
+                        //$row=mysqli_fetch_assoc($record);?>
+                        <div class="friend">
+                        <?php echo $record["name"];
+                    } 
+                    ?>
+                    <button class="btn1" type="button">Message</button>
+                </div>
+            </div>
         </div>
         <div class="pannel center">
             <div class="name-bar"><?php echo $_SESSION["name"]  ?>
