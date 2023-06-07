2efbf382b8c21fa9eb232c7041724ac8c0d2baed
Riddhick <riddhick14@gmail.com>
2023-06-07 08:09:08 +0530

    Message - Database Integrated

--- php/send_message.php
+++ php/send_message.php
@@ -2,9 +2,14 @@
     $data = json_decode(file_get_contents("php://input"), true);
     session_start();
     $user_id=$_SESSION["id"];
+    $mysqli=require __DIR__."/db_connection.php";
 
     $receive_id=$data['receiveId'];
     $message=$data['message'];
+    $date=date('Y/m/d H:i:s');
 
+
+    $sql="INSERT into Message values('$user_id','$receive_id','$message','$date')";
+    mysqli_query($conn,$sql);
     echo $receive_id.$message.$user_id;
 ?>
